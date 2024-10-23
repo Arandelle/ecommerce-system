5cbda44508a29691ba7855fafd568ecf88818308
Arandelle <topaguintsarandell@gmail.com>
2024-10-23 22:02:48 +0800

    Add tailwindcss

---
+++ src/InventoryManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InventoryManagement = () => {
+  return (
+    <h1 className="text-green-500 text-3xl">Hello world</h1>
+  )
+}
+
+export default InventoryManagement
